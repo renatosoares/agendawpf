9d70bf92cf1fb18b04647d85d318ad6f7ed0151d
Renato Soares <renatosoarespro@hotmail.com>
2015-03-02 19:36:08 -0300

    arquivo que falta 'JanelaAdicionar.xaml.cs'

---
+++ TelaPrincipal/JanelaAdicionar.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TelaPrincipal
+{
+    /// <summary>
+    /// Interaction logic for JanelaAdicionar.xaml
+    /// </summary>
+    public partial class JanelaAdicionar : Window
+    {
+        public Compromissos LcompAdd;
+        public JanelaAdicionar()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancelar(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public void Referenciar(Compromissos cs)
+        {
+            this.LcompAdd = cs;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Compromisso c = new Compromisso();
+            c.Nome = TB1A.Text;
+            c.Data = DateTime.Parse(DatePicker.Text);
+            c.Urgente = (bool)CheckBox1.IsChecked;
+            c.HInicial = TB2AHInicial.Text;
+            c.HFinal = TB3AHFinal.Text;
+
+
+            
+            
+
+          //  if (c.HFinal > TB2AHInicial.Text) { 
+
+            
+
+
+            
+
+
+        }
+
+
+
+    }
+}
